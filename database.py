3fe998055f13bfdce8bead095c4e99cf86437437
Bhagyesh Dhanashre <138644030+BhagyeshDhanashre@users.noreply.github.com>
2025-11-24 21:35:10 +0530

    Update database.py

--- database.py
+++ database.py
@@ -1,10 +1,23 @@
 import psycopg2
+import urllib.parse as urlparse
+
+NEON_URL = "postgresql://neondb_owner:npg_FpezTLm5bX2Z@ep-noisy-leaf-a11kkcfa-pooler.ap-southeast-1.aws.neon.tech/neondb?sslmode=require&channel_binding=require"
 
 def get_connection():
+    # Parse the connection string
+    result = urlparse.urlparse(NEON_URL)
+    
+    username = result.username
+    password = result.password
+    database = result.path[1:]
+    hostname = result.hostname
+    port = result.port
+    
     return psycopg2.connect(
-        dbname="fastapi",
-        user="postgres",
-        password=83100,
-        host="localhost",
-        port="5432"
+        dbname=database,
+        user=username,
+        password=password,
+        host=hostname,
+        port=port,
+        sslmode="require"    # 🔥 Neon requires SSL
     )
